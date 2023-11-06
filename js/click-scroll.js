c3b803c1bbe23c7b3599f80f83e903d216c3a130
Katie Stein <katieg@oper.ly>
2023-11-06 12:32:46 -0500

    updates to tutorial

--- js/click-scroll.js
+++ js/click-scroll.js
@@ -6,7 +6,7 @@ var sectionArray = [1, 2, 3, 4, 5];
 $.each(sectionArray, function(index, value){
           
      $(document).scroll(function(){
-         var offsetSection = $('#' + 'section_' + value).offset().top - 75;
+         var offsetSection = $('#' + 'section_' + value).offset() - 75;
          var docScroll = $(document).scrollTop();
          var docScroll1 = docScroll + 1;
          
@@ -19,14 +19,6 @@ $.each(sectionArray, function(index, value){
          }
          
      });
-    
-    $('.click-scroll').eq(index).click(function(e){
-        var offsetClick = $('#' + 'section_' + value).offset().top - 75;
-        e.preventDefault();
-        $('html, body').animate({
-            'scrollTop':offsetClick
-        }, 300)
-    });
     
 });
 
